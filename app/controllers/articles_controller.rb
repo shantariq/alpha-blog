b03272b4b300b6d45b2583175ab060d902185bcd
shan tariq <shantariqcs008@gmail.com>
2016-06-25 10:48:17 -0700

    after show and new method

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+	def new 
+		@article=Article.new
+	end
+
+	def create 
+		#render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		#@article.save
+        #redirect_to_article_path(@article)
+
+        if @article.save
+        	flash[:notice]="your article is created"
+           redirect_to article_path(@article)
+          else
+        	render 'new'
+         
+        end
+	end
+	 def show
+	 	 @article=Article.find(params[:id])
+	 end
+      
+  private
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+	
+end
