af82876169a21c33486400b9e840b9e031f3628b
shan tariq <shantariqcs008@gmail.com>
2016-06-26 09:54:51 -0700

    after crude operation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,17 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update,:show, :destroy]
+    
+      def index
+      	@articles=Article.all
+      end
+
+
 	def new 
 		@article=Article.new
 	end
       
       def edit
-      	@article=Article.find(params[:id])
+      	
       end
 	def create 
 		#render plain: params[:article].inspect
@@ -21,11 +28,18 @@ class ArticlesController < ApplicationController
         end
 	end
 	 def show
-	 	 @article=Article.find(params[:id])
+	 	 
 	 end
       
+      def destroy
+      	 
+      	 @article.destroy
+          flash[:notice]="Article has been deleted"
+      	  redirect_to articles_path
+      end
+
       def update
-      	@article=Article.find(params[:id])
+  
 
 
         if @article.update(article_params)
@@ -38,7 +52,11 @@ class ArticlesController < ApplicationController
 
       end
 
+
   private
+    def  set_article
+    	@article=Article.find(params[:id])
+    end
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end
