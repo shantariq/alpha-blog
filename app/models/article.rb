f86bfaa41853619bbdb890e6fc75476ebbdbb744
shan tariq <shantariqcs008@gmail.com>
2016-06-27 17:18:35 -0700

    done adding style to index

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, length: { minimum:5 , maximum:40 }
-	validates :description, presence: true, length: { minimum:10 , maximum:100 }
+	validates :title, presence: true, length: { minimum:5 , maximum:80 }
+	validates :description, presence: true, length: { minimum:10 , maximum:1000}
 
 end
